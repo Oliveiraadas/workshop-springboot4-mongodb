082cec2049eb6324ca5ac01cbb6b0bf10dc8ef4c
Oliveiraadas <oliveiraadas@gmail.com>
2026-05-27 14:22:55 -0300

    Obtendo um post por id

---
+++ src/main/java/com/oliveiracesar/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.oliveiracesar.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.oliveiracesar.workshopmongo.domain.Post;
+import com.oliveiracesar.workshopmongo.repository.PostRepository;
+import com.sun.jdi.ObjectCollectedException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Post user = repo.findById(id).orElse(null);
+		if (user == null) {
+			throw new ObjectCollectedException("Objeto não enontrado");
+		}
+		return user;
+	}
+}
