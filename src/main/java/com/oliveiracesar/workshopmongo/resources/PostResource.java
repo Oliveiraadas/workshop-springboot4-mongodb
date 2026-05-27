ac5ec7868a50571f8ad043b499c37977affb1efa
Oliveiraadas <oliveiraadas@gmail.com>
2026-05-27 15:37:07 -0300

    Consulta simples com query methods

--- src/main/java/com/oliveiracesar/workshopmongo/resources/PostResource.java
+++ src/main/java/com/oliveiracesar/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.oliveiracesar.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oliveiracesar.workshopmongo.domain.Post;
+import com.oliveiracesar.workshopmongo.resources.util.URL;
 import com.oliveiracesar.workshopmongo.services.PostService;
 
 @RestController
@@ -22,4 +26,16 @@ public class PostResource {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+    
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+        // 💡 CORREÇÃO: Usa o utilitário URL para decodificar o texto que veio da URL
+        text = URL.decodeParam(text);
+        
+        // Busca a lista de posts que contêm o texto no título
+        List<Post> list = service.findByTitle(text);
+        
+        return ResponseEntity.ok().body(list);
+    }
+
 }
